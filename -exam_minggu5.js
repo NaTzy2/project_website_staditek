82d8fe76af042119cb4468deaf0e10428a7a60e7
NaTzy2 <naufalm061@gmail.com>
2022-10-31 09:40:45 +0700

    upload pertama

---
+++ -exam_minggu5.js
@@ -0,0 +1,6 @@
+$('.main-carousel').flickity({
+
+    cellAlign: 'left',
+    wrapAround: true,
+    freeScroll: true
+    })
---
+++ -signup.js
@@ -0,0 +1,8 @@
+document.getElementById('sign').addEventListener('click', function(){
+    alert('kontol')
+})
+
+
+document.getElementById('log').addEventListener('click', function(){
+    alert('kontol')
+})
